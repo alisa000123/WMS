d1f56caf53cdd4a43affe37ff18dc9375bfb443d
John Doe <johndoe@example.com>
2026-05-05 12:03:31 +0300

    add gaps

--- README.md
+++ README.md
@@ -1,48 +1,18 @@
-# Warehouse flow
-Supplier → Warehouse → Customer
+##Tier 1
+#Adding an item
+1. The system searches for the first free coordinate (x, y) in the warehouse.
+2. The system checks positions sequentially until it finds an empty one.
+3. If a free position is found:
+    * The block is moved to that coordinate.
+    * The position is marked as occupied.
+    * The coordinate is stored in the system.
+4. If no free position is available:
+    * The system reports that the warehouse is full.
 
-System must answer:
-What is in stock?
-What leaves the warehouse?
-What remains?
-
-# Three Levels of Complexity
-## Tier 1 (required to pass)
-Track quantities (bulk storage)
-
-## Tier 2
-FIFO batches (oldest first)
-
-## Tier 3
-Individual item tracking
-
-# Workflow
-1. Understand the problem
-2. Define requirements
-3. Design structure
-4. Implement
-5. Test
-6. Improve
-
-# Requirements for all tiers
-The system must:
-- User Interface
-  - Indicates the stock state
-    - Item count
-    - Item info (depending on the tier)
-  - Controls for operating the stock
-    - Adding item
-    - Removing item (according to the tier requirements)
-- Usage
-  - Single run operation. All actions must be able to performed by single launch of the program. The launch of the simulator is excluded from this requirement.
-  - The operation of the software must be possible without seeing the source code
-- Compatibility
-  - The program must be compatible with the real device in the lab and the simulator. The demonstrations are made with the simulator.
-- Software architecture
-  - The source code must be clear and readable
-  - Object oriented programming is to be used
-  - The software needs to be documented in a proper way so that it can be understood
-- Source code management
-  - The commits are made in consistent manner
-  - The stable versions are clearly marked
-  - Each Tier has their own branch in Git
+#Removing an item
+1. The system searches for the first occupied coordinate.
+2. The system selects the first occupied position based on storage order.
+3. The block is removed from that coordinate.
+4. The position is marked as free.
+5. If no occupied positions exist:
+    * The system reports that the warehouse is empty.
--- main.py
+++ main.py
@@ -1,108 +1,127 @@
+# Import libraries
 from py_ads_client import ADSClient, ADSSymbol, BOOL, LREAL
 from time import sleep
+
+# ADS symbols used to send transfer commands to the PLS simulator
 REMOTE_TRANSFER_ITEM = ADSSymbol("Remote.transfer_item", BOOL)
 REMOTE_SRC_X = ADSSymbol("Remote.src_x", LREAL)
 REMOTE_SRC_Y = ADSSymbol("Remote.src_y", LREAL)
 REMOTE_DST_X = ADSSymbol("Remote.dst_x", LREAL)
 REMOTE_DST_Y = ADSSymbol("Remote.dst_y", LREAL)
 
+# PLC connection settings
 PLC_IP = "127.0.0.1"
 PLC_NET_ID = "127.0.0.1.1.1"
 PLC_PORT = 851
 LOCAL_NET_ID = "127.0.0.1.1.2"
 
+# Fixed coordinate of the transfer slot / home pallet area
 TRANSFER_SLOT_CENTER_X = 160.0
 TRANSFER_SLOT_CENTER_Y = 410.0
 
+# Storage area limits
 AREA_MIN_X = 0.0
 AREA_MIN_Y = 0.0
 AREA_MAX_X = 400.0
 STORAGE_MAX_Y = 300.0
 
+# Block size
 BLOCK_SIZE_MM = 60.0
 BLOCK_HALF_SIZE_MM = BLOCK_SIZE_MM / 2.0
 
 
 class Warehouse:
     def __init__(self, ads_client):
-        self.ads = ads_client
-        self.positions = []
-        self.occupied = {}
-
+        self.ads = ads_client # Store ADS client connection
+        self.positions = [] # List of all possible storage coordinates
+        self.occupied = {} # Dictionary that stores whether each coordinate is occupied
+        # Generate all valid storage positions in a grid
         x = AREA_MIN_X + BLOCK_HALF_SIZE_MM
         while x <= AREA_MAX_X - BLOCK_HALF_SIZE_MM:
             y = AREA_MIN_Y + BLOCK_HALF_SIZE_MM
             while y <= STORAGE_MAX_Y - BLOCK_HALF_SIZE_MM:
                 pos = (x, y)
-                self.positions.append(pos)
-                self.occupied[pos] = False
-                y += BLOCK_SIZE_MM
-            x += BLOCK_SIZE_MM
+                self.positions.append(pos) # Add coordinate to the position list
+                self.occupied[pos] = False # Initially every position is free
+                y += BLOCK_SIZE_MM + 20
+            x += BLOCK_SIZE_MM +20
 
     def show(self):
-        occupied = sum(1 for value in self.occupied.values() if value)
+        # Count how many positions are currently occupied
+        occupied = sum(1 for value in self.occupied.values() if value) 
         total = len(self.positions)
 
+        # Print storage information
         print("\n--- STORAGE STATUS ---")
         print(f"Total positions: {total}")
         print(f"Occupied: {occupied}")
         print(f"Free: {total - occupied}")
 
     def transfer(self, src_x, src_y, dst_x, dst_y):
+        # Send source coordinates to the PLC
         self.ads.write_symbol(REMOTE_SRC_X, src_x)
         self.ads.write_symbol(REMOTE_SRC_Y, src_y)
+        # Send destination coordinates to the PLC
         self.ads.write_symbol(REMOTE_DST_X, dst_x)
         self.ads.write_symbol(REMOTE_DST_Y, dst_y)
         sleep(0.1)
+        # Send a short pulse to start the transfer
         self.ads.write_symbol(REMOTE_TRANSFER_ITEM, True)
         sleep(0.1)
         self.ads.write_symbol(REMOTE_TRANSFER_ITEM, False)
 
     def add_block(self):
+        # Search for the first free storage position
         for pos in self.positions:
             if not self.occupied[pos]:
                 x, y = pos
+                # Send pallet to imaging
                 self.ads.write_symbol(ADSSymbol("Remote.send_pallet", BOOL), True)
-
                 sleep(0.1)
                 self.ads.write_symbol(ADSSymbol("Remote.send_pallet", BOOL), False)
                 sleep(0.5)
+                # Release pallet from imaging 
                 self.ads.write_symbol(ADSSymbol("Remote.release_from_imaging", BOOL), True)
                 sleep(0.1)
                 self.ads.write_symbol(ADSSymbol("Remote.release_from_imaging", BOOL), False)
                 sleep(0.5)
-
+                # Move block from transfer to storage position
                 self.transfer(
                     TRANSFER_SLOT_CENTER_X,
                     TRANSFER_SLOT_CENTER_Y,
                     x,
                     y
                 )
-
+                # Mark position as occupied
                 self.occupied[pos] = True
                 print(f"Block moved to {pos}")
 
                 sleep(0.5)
+
+                # Return the pallet back to home
                 self.ads.write_symbol(ADSSymbol("Remote.return_pallet", BOOL), True)
                 sleep(0.1)
                 self.ads.write_symbol(ADSSymbol("Remote.return_pallet", BOOL), False)
                 return
 
-        print("Warehouse is full")
+        print("Warehouse is full")# If no free position was found
 
     def remove_block(self):
+        # Search for the first occupied storage position
         for pos in self.positions:
             if self.occupied[pos]:
                 x, y = pos
+                # Send pallet to imaging
                 self.ads.write_symbol(ADSSymbol("Remote.send_pallet", BOOL), True)
                 sleep(0.1)
                 self.ads.write_symbol(ADSSymbol("Remote.send_pallet", BOOL), False)
                 sleep(0.5)
+                # Release pallet from imaging
                 self.ads.write_symbol(ADSSymbol("Remote.release_from_imaging", BOOL), True)
                 sleep(0.1)
                 self.ads.write_symbol(ADSSymbol("Remote.release_from_imaging", BOOL), False)
                 sleep(0.5)
-                
+                # Move block from storage position to the transfer
                 self.transfer(
                     x,
                     y,
@@ -110,27 +129,31 @@ class Warehouse:
                     TRANSFER_SLOT_CENTER_Y
                 )
                 sleep(0.5)
+                # Return pallet back to home
                 self.ads.write_symbol(ADSSymbol("Remote.return_pallet", BOOL), True)
                 sleep(0.1)
                 self.ads.write_symbol(ADSSymbol("Remote.return_pallet", BOOL), False)
-
+                # Mark this position as free
                 self.occupied[pos] = False
                 print(f"Block removed from {pos}")
                 return
 
-        print("Warehouse is empty")
+        print("Warehouse is empty")# If no occupied position was found
 
 
 def main():
+    # Create ADS client
     ads = ADSClient(local_ams_net_id=LOCAL_NET_ID)
+    # Open connection to PLC simulator
     ads.open(
         target_ip=PLC_IP,
         target_ams_net_id=PLC_NET_ID,
         target_ams_port=PLC_PORT
     )
     
-    warehouse = Warehouse(ads)
+    warehouse = Warehouse(ads)# Create warehouse object
 
+    # Terminal menu
     while True:
         print("\n--- MENU ---")
         print("1. Show status")
@@ -150,6 +173,7 @@ def main():
             warehouse.remove_block()
 
         elif choice == "4":
+            # Close ADS connection before exiting
             ads.close()
             print("Goodbye")
             break
@@ -157,6 +181,6 @@ def main():
         else:
             print("Wrong choice")
 
-
+# Start the program
 if __name__ == "__main__":
     main()
